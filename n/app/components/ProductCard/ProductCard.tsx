bc07059953131c6585cc3c4df591ed178d62706c
RayIT <rmarx@rayit.com>
2024-11-26 23:10:13 +0100

    looking into tailwind

--- n/app/components/ProductCard/ProductCard.tsx
+++ n/app/components/ProductCard/ProductCard.tsx
@@ -4,7 +4,7 @@ import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='border-solid border-2 p-5 my-5 border-spacing-2'>
       <AddToCard />
     </div>
   )
